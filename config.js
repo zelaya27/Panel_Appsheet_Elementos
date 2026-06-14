5d0fad689f2c4b6f17851306379c010d0e9dd4c3
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-13 20:50:58 -0600

    Update APPSHEET_APP_NAME in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
   URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbyq2ZPsMUCnTFhHSjVvGZPr4-FiinnIByOwogTgridKVzUhp2P1VMddsey4BK6kqOp3/exec",
-  APPSHEET_APP_NAME: "APP_INSPECCIONES"
+  APPSHEET_APP_NAME: "APP_INSPECCIONES-236810661"
 };
