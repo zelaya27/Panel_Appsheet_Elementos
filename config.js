ed8b9caa6a293b7c7d6c061ad7a39ea93f98be94
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-13 20:46:59 -0600

    Update URL_APPS_SCRIPT and add APPSHEET_APP_NAME

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const CONFIG = {
-  URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycby56GK8eaIze1rcKLUFHnDKprRqCVEd8_EPKJkZ4LVjH5pIE4TWcMiaf2AsxO2Lr-0/exec"
+  URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbyq2ZPsMUCnTFhHSjVvGZPr4-FiinnIByOwogTgridKVzUhp2P1VMddsey4BK6kqOp3/exec",
+  APPSHEET_APP_NAME: "APP_INSPECCIONES"
 };
