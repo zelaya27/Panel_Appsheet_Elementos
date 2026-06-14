499c00a45fbd88a4a9d82ff073845ff26e3c4bf0
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-13 22:23:34 -0600

    Update URL_APPS_SCRIPT to new endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbyq2ZPsMUCnTFhHSjVvGZPr4-FiinnIByOwogTgridKVzUhp2P1VMddsey4BK6kqOp3/exec",
+  URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbwloTKRg1j-noRwHJ8s1lM3tDKrWmem6pit0q768Mt9XtbUX3-iNbBt4s1OVedeSmN_/exec",
   APPSHEET_APP_NAME: "APP_INSPECCIONES-236810661"
 };
