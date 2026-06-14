14ef5b99b309f2ed97d5df05ae6fc58457483152
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-13 22:35:09 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbwloTKRg1j-noRwHJ8s1lM3tDKrWmem6pit0q768Mt9XtbUX3-iNbBt4s1OVedeSmN_/exec",
+  URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbz3Vn3oHCfioiFEEj7dW7DJ1CUwvJR7rHrzSmOHw7dXs_9D4PfWU5DIFzgHjGbsgBG0/exec",
   APPSHEET_APP_NAME: "APP_INSPECCIONES-236810661"
 };
