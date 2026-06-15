30ba28054eeb89edff48ebcdcd7ba4b68620a092
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-15 16:13:53 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
 const CONFIG = {
-  URL_APPS_SCRIPT:
-    "https://script.google.com/macros/s/AKfycbxWgSDOn__o7nXG1xbt6wnO96Yj1RBGlXLIup2x8zkmb6KE7V8l6ncF9_njJ8xheJ0-/exec",
-
-  APPSHEET_APP_NAME:
-    "APP_INSPECCIONES-236810661"
+  URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbxWgSDOn__o7nXG1xbt6wnO96Yj1RBGlXLIup2x8zkmb6KE7V8l6ncF9_njJ8xheJ0-/exec",
+  APPSHEET_APP_NAME: "APP_INSPECCIONES-236810661"
 };
 
 window.MAPAS_SECTOR = {
-
   "JUTICALPA": {
     nombre: "Mapa base Juticalpa",
     mid: "1AD2wEag68h4hiBy5R7fbW9Ir-6BmsCs",
@@ -22,6 +18,54 @@ window.MAPAS_SECTOR = {
     lat: 15.276242326616291,
     lng: -87.43169250000001,
     zoom: 9
-  }
+  },
 
+  "CATACAMAS": {
+    nombre: "Mapa base Catacamas",
+    lat: 14.8391,
+    lng: -85.8954,
+    zoom: 11
+  },
+
+  "CHOLUTECA": {
+    nombre: "Mapa base Choluteca",
+    lat: 13.3003,
+    lng: -87.1908,
+    zoom: 11
+  },
+
+  "TEGUCIGALPA": {
+    nombre: "Mapa base Tegucigalpa",
+    lat: 14.0723,
+    lng: -87.1921,
+    zoom: 11
+  },
+
+  "DANLI": {
+    nombre: "Mapa base Danlí",
+    lat: 14.0333,
+    lng: -86.5833,
+    zoom: 11
+  },
+
+  "TOCOA": {
+    nombre: "Mapa base Tocoa",
+    lat: 15.6833,
+    lng: -86.0000,
+    zoom: 11
+  },
+
+  "LA CEIBA": {
+    nombre: "Mapa base La Ceiba",
+    lat: 15.7797,
+    lng: -86.7931,
+    zoom: 11
+  },
+
+  "COMAYAGUA": {
+    nombre: "Mapa base Comayagua",
+    lat: 14.4514,
+    lng: -87.6375,
+    zoom: 11
+  }
 };
