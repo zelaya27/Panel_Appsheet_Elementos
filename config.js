c654c6258811148a05cd3568807105f6e7bd991e
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-15 16:04:48 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,27 @@
 const CONFIG = {
-  URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbxove1QEAPbMpjfxnR4E8P9myG74IDLP2-ygvraKKnpCRMe43Ndo_Ik1ExaxgHNcHE/exec",
-  APPSHEET_APP_NAME: "APP_INSPECCIONES-236810661"
+  URL_APPS_SCRIPT:
+    "https://script.google.com/macros/s/AKfycbxWgSDOn__o7nXG1xbt6wnO96Yj1RBGlXLIup2x8zkmb6KE7V8l6ncF9_njJ8xheJ0-/exec",
+
+  APPSHEET_APP_NAME:
+    "APP_INSPECCIONES-236810661"
+};
+
+window.MAPAS_SECTOR = {
+
+  "JUTICALPA": {
+    nombre: "Mapa base Juticalpa",
+    mid: "1AD2wEag68h4hiBy5R7fbW9Ir-6BmsCs",
+    lat: 14.958349515262615,
+    lng: -86.14916699999998,
+    zoom: 9
+  },
+
+  "EL PROGRESO": {
+    nombre: "Mapa base El Progreso",
+    mid: "1g7RknupgCVrUFtKSJwfEUbqaQK8b-r8",
+    lat: 15.276242326616291,
+    lng: -87.43169250000001,
+    zoom: 9
+  }
+
 };
